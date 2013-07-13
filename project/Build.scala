955595a3dc053bdffb1d27f8219439de0e046838
randhindi <rand.hindi@snips.net>
2013-07-14 01:24:07 +0200

    Added one-jar config

--- project/Build.scala
+++ project/Build.scala
@@ -1,11 +1,12 @@
 import sbt._
 import Keys._
+import com.github.retronym.SbtOneJar
 
 object ApplicationBuild extends Build {
    
     //The main project
     val main = Project(id = "jerkson", base = file(".")).settings(
-
+    	(Seq(exportJars := true) ++ Defaults.defaultSettings ++ SbtOneJar.oneJarSettings):_*
     )
 
 }
